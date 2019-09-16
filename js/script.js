b797caa5826363ecdce8400c054b1d6e7ebc5b8f
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-09-16 19:53:17 +0200

    Fix email, change logo, add hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 $('#years').animateNumber({ number: 20 });
+
+const hamburger = document.querySelector("#hamburger");
+
+function showMenu() {
+    const menu = document.querySelector("#dropdownMenu");
+    menu.classList.toggle("invisible");
+}
+
+hamburger.addEventListener('click', showMenu)
